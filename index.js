e5a7b0a7f89268504c63dd45a5f8d92f6ff7adc7
Devansh <kdevanshsharma23@gmail.com>
2019-05-15 21:52:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+   
+}
+function myFunctio() {
+  var popup = document.getElementById("pop");
+  popup.classList.toggle("show");
+}
+function resize(){
+var w = window.outerWidth;
+  var h = window.outerHeight;
+  var i,j;
+  
+
+}
